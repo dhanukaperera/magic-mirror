af48ef9c4aa217ddfa1550b2e3250e0b9ee5e6af
RPA BOT10 <rpa10@nations.int>
2017-10-04 13:28:36 +0530

    news feed connected with firebase

---
+++ src/models/newsfeed.interface.ts
@@ -0,0 +1,7 @@
+export interface NewsFeed_I {
+    $key?: string;
+    display : boolean;
+    interval : Number;
+    sortBy : string;
+    source : string
+}
--- src/pages/newsfeed/newsfeed.ts
+++ src/pages/newsfeed/newsfeed.ts
@@ -1,19 +1,43 @@
-import { Component } from '@angular/core';
-import {  NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController, NavParams} from 'ionic-angular';
+import {AngularFireDatabase, FirebaseObjectObservable} from 'angularfire2/database';
+import {Subscription} from 'rxjs/Subscription';
+import {NewsFeed_I} from '../../models/newsfeed.interface';
 
-
-@Component({
-  selector: 'page-newsfeed',
-  templateUrl: 'newsfeed.html',
-})
+@Component({selector: 'page-newsfeed', templateUrl: 'newsfeed.html'})
 export class NewsfeedPage {
 
-  private display: boolean;
-  private sortBy: string;
-  private source: string;
-  private interval: string;
-  
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  NewsFeedSubscription : Subscription;
+  NewsFeedRef$ : FirebaseObjectObservable < NewsFeed_I >;
+  NewsFeed = {};
+
+  constructor(public navCtrl : NavController, public navParams : NavParams,private database : AngularFireDatabase) {
+    this.NewsFeedRef$ = this
+    .database
+    .object(`/db/newsfeed/`);
+
+    this.NewsFeedSubscription = this
+    .NewsFeedRef$
+    .subscribe(newsfeed => {
+      this.NewsFeed = newsfeed;
+      console.log(newsfeed);
+    });
+  }
+
+  showNewsFeed(newsfeed : NewsFeed_I): void{
+    this.NewsFeedRef$.update(newsfeed);
+  }
+
+  changeSortBy(newsfeed : NewsFeed_I) : void {
+    this.NewsFeedRef$.update(newsfeed);
+  }
+
+  changeSource(newsfeed : NewsFeed_I ): void {
+    this.NewsFeedRef$.update(newsfeed);
+  }
+
+  changeInterval(newsfeed : NewsFeed_I) : void {
+    this.NewsFeedRef$.update(newsfeed);
   }
 
   ionViewDidLoad() {
